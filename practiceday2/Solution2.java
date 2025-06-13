f99982b62e89addf0b630d9c4177f63ee653d794
AKSHIT <akshitgoyal545@gmail.com>
2025-06-13 09:27:55 +0530

    day2

---
+++ practiceday2/Solution2.java
@@ -0,0 +1,16 @@
+class Solution2{
+    public static void main(String[] args) {
+        
+        int costPrice = 129;
+        int sellingPrice = 191; 
+
+        int profit = sellingPrice - costPrice; // Winst berekenen
+        double profitPercentage = ((double) profit / costPrice) * 100; 
+
+        
+        System.out.println("The Cost Price is INR " + costPrice + " and the Selling Price is INR " + sellingPrice + "\n" +
+                           "The Profit is INR " + profit + " and the Profit Percentage is " + String.format("%.2f", profitPercentage));
+    }
+}
+
+        
