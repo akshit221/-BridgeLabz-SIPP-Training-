4324b9b848a105da8ac9a9ce04a572dbfbee33b4
AKSHIT <akshitgoyal545@gmail.com>
2025-06-16 12:49:44 +0530

    Day_3

---
+++ Day_3/Solution5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Solution5{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n = sc.nextInt();
+        if(n%5 == 0){
+                System.out.println(true);
+        }else{
+            System.out.println("false");
+        }
+
+
+
+
+        
+    }
+}
